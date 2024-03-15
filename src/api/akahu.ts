d93ac25c9c54cec8bfc8576001e3edb4c409261c
Sam Stokes <sam@stellarlibrary.com>
2024-03-15 16:42:39 +1300

    initial setup

--- src/api/akahu.ts
+++ src/api/akahu.ts
@@ -1,7 +1,4 @@
 export const getUserDetails = async () => {
-  const userToken = 'user_token_cltns8j3l000208jzeuvmgsd1'
-  const appId = 'app_token_cltns8j3l000108jze2lnc7qc'
-
   const response = await fetch('https://api.akahu.io/v1/me', {
     method: 'GET',
     headers: new Headers({
@@ -11,9 +8,6 @@ export const getUserDetails = async () => {
   })
 
   const data = await response.json()
-  console.log('====================================')
-  console.log(data)
-  console.log('====================================')
 
   return data
 }
